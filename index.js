b841f3d7f14dda17eabe52dd45b3f8be1056b0ad
Rohan Batra <rohanbatra24@gmail.com>
2021-10-15 18:16:34 -0400

    add route for creating new post

--- index.js
+++ index.js
@@ -3,16 +3,14 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
-import postsRouter from "./routes/posts.js";
+import postsRoutes from "./routes/posts.js";
 
 const app = express();
-
-app.use("/posts", postsRouter);
-
+app.use(cors());
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
-app.use(cors());
+app.use("/posts", postsRoutes);
 
 const CONNECTION_URL =
   "mongodb+srv://rohanbatra:5AyDRqiz7E97heyR@cluster0.ybogw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
