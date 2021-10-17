66620d7ba11c6ea41aeb57ac0dee0ddd4ab035f8
Rohan Batra <rohanbatra24@gmail.com>
2021-10-16 22:36:48 -0400

    add route for liking post

--- controllers/posts.js
+++ controllers/posts.js
@@ -54,3 +54,23 @@ export const deletePost = async (req, res) => {
 
   res.json({ message: "Post deleted successfully" });
 };
+
+export const likePost = async (req, res) => {
+  const { id } = req.params;
+
+  // check if it is a valid mongoose id
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send("No post found with that ID");
+
+  const post = PostMessage.findById(id);
+
+  const updatedPost = await PostMessage.findByIdAndUpdate(
+    id,
+    { likeCount: post.likeCount++ },
+    {
+      new: true,
+    }
+  );
+
+  res.json(updatedPost);
+};
